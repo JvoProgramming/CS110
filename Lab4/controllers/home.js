30d932f4d42282702362eeb1e417b8751e0f17f4
JvoProgramming <jvoprogramming@gmail.com>
2021-05-25 20:37:48 -0700

    completed chat app

--- Lab4/controllers/home.js
+++ Lab4/controllers/home.js
@@ -1,10 +1,15 @@
 // Controller handler to handle functionality in home page
 
+const Rooms = require("../models/Rooms");
+
 // Example for handle a get request at '/' endpoint.
 
 function getHome(request, response){
   // do any work you need to do, then
-  response.render('home', {title: 'home'});
+  Rooms.find().lean().then(items => 
+  {
+    response.render('home', {title: 'home', rooms: items, isAvailable: true});
+  })
 }
 
 module.exports = {
